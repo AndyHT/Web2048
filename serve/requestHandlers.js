cf9909bfdd796a52731ec7ee53c1862332d9bfef
Teng <843018739@qq.com>
2015-12-24 10:44:18 +0800

    completed server: can return static file to front

--- serve/requestHandlers.js
+++ serve/requestHandlers.js
@@ -4,9 +4,12 @@ function startGame(request, response) {
   //读index.html后返回给前端
   console.log("读index.html后返回给前端");
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("读index.html后返回给前端");
-    response.end();
+  var data = fs.readFileSync('./public/index.html');
+  // console.log("同步读取: " + data.toString());
+
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(data);
+  response.end();
 }
 
 function addNewRecord(request, response) {
@@ -17,11 +20,29 @@ function addNewRecord(request, response) {
   response.end();
 }
 
-function returnFiles(request, response) {
-  //将静态文件资源返回给前端
-  console.log("将静态文件资源返回给前端");
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("将静态文件资源返回给前端");
+function returnFiles(request, response, pathname) {
+
+  var path = "./public" + pathname;
+  console.log("path:" + path);
+
+  // console.log("Method:" + request.method);
+  // console.log("Headers:" + typeof request.headers['accept']);
+
+
+  //判断请求路径是否有文件，如果有文件将静态文件资源返回给前端
+  if (fs.existsSync(path)) {
+    console.log("将静态文件资源返回给前端");
+
+    var data = fs.readFileSync('./public' + pathname);
+    // console.log("pathname:" + data.toString());
+    response.writeHead(200, {"Content-Type": request.headers['accept']});
+    response.write(data);
+  } else {//如果文件不存在返回404
+    console.log("找不到文件:" + pathname);
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not found");
+  }
+
   response.end();
 }
 
--- serve/router.js
+++ serve/router.js
@@ -3,14 +3,7 @@ function route(handle, pathname, response, request) {
   if (typeof handle[pathname] === 'function') {
     handle[pathname](request, response);
   } else {
-    if (true) {//如果pathname对应路径的文件存在，返回路径对应的文件
-      handle["others"](request, response);
-    } else {//如果文件不存在返回404
-      console.log("找不到文件:" + pathname);
-      response.writeHead(404, {"Content-Type": "text/html"});
-      response.write("404 Not found");
-      response.end();
-    }
+    handle["others"](request, response, pathname);
   }
 }
 
